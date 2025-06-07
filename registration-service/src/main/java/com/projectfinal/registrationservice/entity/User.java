64223ec08ea37aea871c4631e004320fe1860506
MarioFilian <mafilian@uce.edu.ec>
2025-06-07 15:26:25 -0500

    feat(entity): 🏗️ create user JPA entity with basic fields

---
+++ registration-service/src/main/java/com/projectfinal/registrationservice/entity/User.java
@@ -0,0 +1,33 @@
+package com.projectfinal.registrationservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
