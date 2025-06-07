a60cd612562d341c8d93802643b2a5e46508db6e
MarioFilian <mafilian@uce.edu.ec>
2025-06-07 17:42:04 -0500

    feat(dto): 📨 include full name and role in user DTO

--- registration-service/src/main/java/com/projectfinal/registrationservice/dto/UserDTO.java
+++ registration-service/src/main/java/com/projectfinal/registrationservice/dto/UserDTO.java
@@ -17,13 +17,24 @@ public class UserDTO {
     @Email(message = "Email should be valid")
     private String email;
 
+    @NotBlank(message = "First name is mandatory")
+    private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+
+    private String role = "USER"; // Default value if not provided
+
     // Constructors
     public UserDTO() {}
 
-    public UserDTO(String username, String password, String email) {
+    public UserDTO(String username, String password, String email, String firstName, String lastName, String role) {
         this.username = username;
         this.password = password;
         this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role != null ? role : "USER";
     }
 
     // Getters and setters
@@ -35,4 +46,13 @@ public class UserDTO {
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
