8e0b27c6f85159190283c2e5c216c232412388cf
MarioFilian <mafilian@uce.edu.ec>
2025-06-07 19:41:02 -0500

    docs(swagger): 📝 add example data descriptions for registration API in UserDTO schema

--- registration-service/src/main/java/com/projectfinal/registrationservice/config/SwaggerConfig.java
+++ registration-service/src/main/java/com/projectfinal/registrationservice/config/SwaggerConfig.java
@@ -3,11 +3,16 @@ package com.projectfinal.registrationservice.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -19,6 +24,15 @@ public class SwaggerConfig {
                         .contact(new Contact()
                                 .name("Equipo de Desarrollo")
                                 .email("dev@projectfinal.com")
-                                .url("https://www.projectfinal.com")));
+                                .url("https://www.projectfinal.com")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter your JWT token in the format: **Bearer &lt;token&gt;**")));
     }
 }
