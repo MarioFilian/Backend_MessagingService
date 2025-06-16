517cf2855c26a18dff6907926d107624e31b2704
MarioFilian <mafilian@uce.edu.ec>
2025-06-15 19:59:00 -0500

    update(service): 🔄 update RegistrationService.java

--- registration-service/src/main/java/com/projectfinal/registrationservice/service/RegistrationService.java
+++ registration-service/src/main/java/com/projectfinal/registrationservice/service/RegistrationService.java
@@ -1,26 +1,33 @@
 package com.projectfinal.registrationservice.service;
 
+import com.projectfinal.registrationservice.dto.TokenResponseDTO;
 import com.projectfinal.registrationservice.dto.UserDTO;
 import com.projectfinal.registrationservice.entity.User;
 import com.projectfinal.registrationservice.repository.UserRepository;
 import com.projectfinal.registrationservice.util.JwtUtil;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 public class RegistrationService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final StringRedisTemplate redisTemplate;
 
-    public RegistrationService(UserRepository userRepository, JwtUtil jwtUtil) {
+    public RegistrationService(UserRepository userRepository, JwtUtil jwtUtil, StringRedisTemplate redisTemplate) {
         this.userRepository = userRepository;
-        this.passwordEncoder = new BCryptPasswordEncoder();
         this.jwtUtil = jwtUtil;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+        this.redisTemplate = redisTemplate;
     }
 
-    public String register(UserDTO userDTO) {
+    public TokenResponseDTO register(UserDTO userDTO) {
         if (userRepository.existsByUsername(userDTO.getUsername())) {
             throw new IllegalArgumentException("Username is already taken.");
         }
@@ -40,6 +47,13 @@ public class RegistrationService {
 
         userRepository.save(newUser);
 
-        return jwtUtil.generateToken(newUser.getUsername());
+        String accessToken = jwtUtil.generateAccessToken(newUser.getUsername(), newUser.getRole());
+        String refreshToken = jwtUtil.generateRefreshToken(newUser.getUsername(), newUser.getRole());
+
+        // Guardar el refresh token en Redis
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set(refreshToken, newUser.getUsername(), Duration.ofDays(7)); // TTL: 7 días
+
+        return new TokenResponseDTO(accessToken, refreshToken);
     }
 }
