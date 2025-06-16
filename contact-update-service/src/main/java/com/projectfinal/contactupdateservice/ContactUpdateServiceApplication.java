f55bf335bc1cf94c68852eb1f5377591551735c1
MarioFilian <mafilian@uce.edu.ec>
2025-06-16 08:51:00 -0500

    feat(update-service): 🎯 initial commit with full contact update microservice in Spring Boot

---
+++ contact-update-service/src/main/java/com/projectfinal/contactupdateservice/ContactUpdateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projectfinal.contactupdateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactUpdateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactUpdateServiceApplication.class, args);
+    }
+
+}
---
+++ contact-update-service/src/test/java/com/projectfinal/contactupdateservice/ContactUpdateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectfinal.contactupdateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactUpdateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
