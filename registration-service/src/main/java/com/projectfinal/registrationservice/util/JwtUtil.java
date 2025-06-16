e0d35b80fb9c96afe38a5bf2b3d3ff48effa8b32
MarioFilian <mafilian@uce.edu.ec>
2025-06-15 19:58:40 -0500

    update(util): 🔄 update JwtUtil.java

--- registration-service/src/main/java/com/projectfinal/registrationservice/util/JwtUtil.java
+++ registration-service/src/main/java/com/projectfinal/registrationservice/util/JwtUtil.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
@@ -20,20 +22,33 @@ public class JwtUtil {
     @Value("${JWT_EXPIRATION_MS}")
     private long jwtExpirationMs;
 
+    @Value("${JWT_REFRESH_EXPIRATION_MS}")
+    private long jwtRefreshExpirationMs;
+
     private SecretKey key;
 
     @PostConstruct
     public void init() {
-        // Decodificar la clave secreta desde hex o base64 a bytes
         byte[] keyBytes = Base64.getDecoder().decode(jwtSecret);
         key = Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public String generateToken(String username) {
+    public String generateAccessToken(String username, String role) {
         return Jwts.builder()
                 .setSubject(username)
+                .claim("role", role)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtRefreshExpirationMs))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
