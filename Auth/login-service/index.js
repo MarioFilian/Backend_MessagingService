f252a008ca50356a90112593ce4e06fe060581f0
MarioFilian <mafilian@uce.edu.ec>
2025-06-24 00:09:53 -0500

    fix(login-service): 🐛 correct variable name for EC2_HOST_LOGIN in Swagger docs URL

--- Auth/login-service/index.js
+++ Auth/login-service/index.js
@@ -14,7 +14,8 @@ app.use('/auth', authRoutes); // Luego tus rutas reales
 
 
 const PORT = process.env.PORT || 3001;
+const EC2_HOST_LOGIN = process.env.EC2_HOST_LOGIN;
 app.listen(PORT, () => {
   console.log(`✅ Login service running on port ${PORT}`);
-  console.log(`📄 Swagger docs available at http://${EC2_HOS_LOGIN}:${PORT}/api-docs`);
+  console.log(`📄 Swagger docs available at http://${EC2_HOST_LOGIN}:${PORT}/api-docs`);
 });
