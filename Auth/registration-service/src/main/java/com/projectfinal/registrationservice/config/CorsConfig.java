0d6c460dd9c30df20fdd331cf942573fc42a3b90
MarioFilian <mafilian@uce.edu.ec>
2025-06-24 00:50:10 -0500

    fix(cors): 🐛 update CORS configuration to use allowedOriginPatterns instead of allowedOrigins

--- Auth/registration-service/src/main/java/com/projectfinal/registrationservice/config/CorsConfig.java
+++ Auth/registration-service/src/main/java/com/projectfinal/registrationservice/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(allowedOrigin)
+                        .allowedOriginPatterns(allowedOrigin) // reemplaza esta línea
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
