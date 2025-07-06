09163f1b5ef8c3dda09192b37301f587374076ca
MarioFilian <mafilian@uce.edu.ec>
2025-07-05 22:03:37 -0500

    chore(mongodb): 🛠️ update MongoDB port configuration to use environment variable and remove test database initialization

--- DB/MongoDB/init-mongo.template.js
+++ DB/MongoDB/init-mongo.template.js
@@ -5,7 +5,3 @@ db = connect("mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@localhost:27017/");
 // Crear base de datos `presence-connect-service`
 db = db.getSiblingDB("presence-connect-service");
 db.createCollection("initCollection");
-
-// Crear base de datos `presence-connect-service-test`
-db = db.getSiblingDB("presence-connect-service-test");
-db.createCollection("initCollection");
