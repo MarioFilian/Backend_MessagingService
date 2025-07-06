098e2a7ba03836c92536f99962dfb2073a469565
MarioFilian <mafilian@uce.edu.ec>
2025-07-06 13:22:25 -0500

    feat(health): ✨ add HealthController and corresponding test for health check endpoint

---
+++ Auth/registration-service/src/test/java/com/projectfinal/registrationservice/HealthControllerTest.java
@@ -0,0 +1,26 @@
+// src/test/java/com/projectfinal/registrationservice/HealthControllerTest.java
+package com.projectfinal.registrationservice;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.projectfinal.registrationservice.controller.HealthController;
+
+@WebMvcTest(HealthController.class)
+public class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHealthEndpoint() throws Exception {
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("ok"));
+    }
+}
