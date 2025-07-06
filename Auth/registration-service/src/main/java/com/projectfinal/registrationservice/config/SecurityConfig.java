c97ab01e23128161603673d669f94e7eda2e107a
MarioFilian <mafilian@uce.edu.ec>
2025-07-06 13:29:43 -0500

    fix(security): 🐛 correct formatting for health endpoint in SecurityConfig

--- Auth/registration-service/src/main/java/com/projectfinal/registrationservice/config/SecurityConfig.java
+++ Auth/registration-service/src/main/java/com/projectfinal/registrationservice/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                                 "/v3/api-docs",
                                 "/swagger-resources/**",
                                 "/webjars/**",
-                                "/health",
+                                "/health"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
