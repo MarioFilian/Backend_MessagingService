49059323e84c97947cffd4f03237923a340468d2
MarioFilian <mafilian@uce.edu.ec>
2025-07-06 13:50:10 -0500

    feat(tests): ✨ add spring-security-test dependency and clean up HealthControllerTest

--- Auth/registration-service/src/test/java/com/projectfinal/registrationservice/HealthControllerTest.java
+++ Auth/registration-service/src/test/java/com/projectfinal/registrationservice/HealthControllerTest.java
@@ -1,27 +1,21 @@
 package com.projectfinal.registrationservice;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import com.projectfinal.registrationservice.controller.HealthController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
-import org.springframework.security.test.context.support.WithMockUser;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(HealthController.class)
-@Import(SecurityConfig.class)  // 👈 si quieres incluir tu SecurityConfig
 public class HealthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser  // 👈 simula usuario autenticado para evitar el 401
     public void testHealthEndpoint() throws Exception {
         mockMvc.perform(get("/health"))
                 .andExpect(status().isOk())
