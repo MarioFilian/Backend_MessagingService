0abeeff2ae7daf89894c3ae6e34478641007773b
MarioFilian <mafilian@uce.edu.ec>
2025-07-05 23:09:07 -0500

    chore(mongodb): 🛠️ add initial MongoDB configuration and initialization script

---
+++ DB/MongoDB2/init-mongo.template.js
@@ -0,0 +1,7 @@
+// init-mongo.template.js
+
+db = connect("mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@localhost:27017/");
+
+// Crear base de datos `presence-connect-service`
+db = db.getSiblingDB("presence-connect-service");
+db.createCollection("initCollection");
